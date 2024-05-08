0d9ec8567d09528a5f09d338ad04d1e4450d0b97
SlayZ121 <slayz9168@gmail.com>
2024-05-08 20:24:37 +0530

    commited

--- market/__init__.py
+++ market/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+app.config['SECRET_KEY']='afa28ecb67dfd9d0f354008d'
 db = SQLAlchemy(app)
-
+bcrypt=Bcrypt(app)
 from market import routes
 from market.models import Item
 
